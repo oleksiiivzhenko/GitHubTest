f3940238702e151ffd8f935e1536b5b42910d83f
oleksiiivzhenko <ivzhenko.oleksii@dteamstudio.com>
2020-02-26 11:06:05 +0200

    commit 1

--- Assets/Scripts/FirstScrupt.cs
+++ Assets/Scripts/FirstScrupt.cs
@@ -7,7 +7,7 @@ public class FirstScrupt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //1
     }
 
     // Update is called once per frame
